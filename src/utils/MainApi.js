00a7a4b8fbfd6940330ea1eb0ff963913ca7c7b5
Дмитрий Ледовских <dmitryledovskih1@yandex.ru>
2026-02-15 14:48:58 +0700

    fix

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3001";
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 export const register = async (name, email, password) => {
   const res = await fetch(`${BASE_URL}/signup`, {

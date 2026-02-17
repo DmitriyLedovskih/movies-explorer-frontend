d8afad86cc39da7173f043df064636f7f3fe2f40
Дмитрий Ледовских <dmitryledovskih1@yandex.ru>
2026-02-17 21:29:26 +0700

    fix

--- src/components/NavTab/NavTab.jsx
+++ src/components/NavTab/NavTab.jsx
@@ -2,22 +2,42 @@ import React from "react";
 import "./NavTab.css";
 
 const NavTab = () => {
+  const handleClick = (evt, id) => {
+    evt.preventDefault();
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <nav className="nav-tab">
       <div className="container">
         <ul className="nav-tab__list main-list">
           <li className="nav-tab__item">
-            <a href="#about-project" className="nav-tab__link main-link">
+            <a
+              href="#about-project"
+              className="nav-tab__link main-link"
+              onClick={(evt) => handleClick(evt, "about-project")}
+            >
               О проекте
             </a>
           </li>
           <li className="nav-tab__item">
-            <a href="#techs" className="nav-tab__link main-link">
+            <a
+              href="#techs"
+              className="nav-tab__link main-link"
+              onClick={(evt) => handleClick(evt, "techs")}
+            >
               Технологии
             </a>
           </li>
           <li className="nav-tab__item">
-            <a href="#about-me" className="nav-tab__link main-link">
+            <a
+              href="#about-me"
+              className="nav-tab__link main-link"
+              onClick={(evt) => handleClick(evt, "about-me")}
+            >
               Студент
             </a>
           </li>
